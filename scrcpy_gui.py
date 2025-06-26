7cab045f1aea45d1a66355a4e0442eef78984cdd
jtliaw <jenntongliaw@yahoo.com.my>
2025-06-26 16:46:36 +0800

    Update scrcpy_gui.py

--- scrcpy_gui.py
+++ scrcpy_gui.py
@@ -181,7 +181,7 @@ class PCFileBrowser(tk.Toplevel):
 
 # --- 语言字典 ---
 LANGUAGES = {
-    "app_title": {"zh": "Artocarpus (Scrcpy 图形界面) v5.0", "en": "ARtocarpus (Scrcpy GUI) v5.0"},
+    "app_title": {"zh": "Artocarpus (Scrcpy 图形界面) v5.1", "en": "ARtocarpus (Scrcpy GUI) v5.1"},
     "tab_single_device": {"zh": "精细控制", "en": "Fine Control"},
     "tab_multi_device": {"zh": "多设备预设", "en": "Multi-Device Profiles"},
     "profile_label": {"zh": "预设 P{}", "en": "Profile P{}"},
